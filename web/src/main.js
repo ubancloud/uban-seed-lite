056c12a58959f1392125822ca19ee3ed61d9ba20
xuyongchao <mail_xyc@sina.com>
2023-03-20 01:15:26 +0800

    Initial commit

--- web/src/main.js
+++ web/src/main.js
@@ -6,9 +6,9 @@ import ConfigDefault from "@/config/config.default";
 import ConfigMenu from "@/config/config.menu";
 import ConfigUrl from "@/config/config.url";
 
-import ClapVueCoreLite from "@clapjs/vue-core-lite";
+import UbanVueLite from "@ubancloud/vue";
 
-Vue.use(ClapVueCoreLite,{...ConfigDefault,axios:ConfigAxios,url:ConfigUrl,menu:ConfigMenu})
+Vue.use(UbanVueLite,{...ConfigDefault,axios:ConfigAxios,url:ConfigUrl,menu:ConfigMenu})
 
 import i18n from "@/lib/i18n";
 import router from "@/lib/router";
